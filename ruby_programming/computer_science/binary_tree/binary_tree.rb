0e19618a553751f4b3296fb30f21ea4e8b9f8efd
Laura <laurabennett701@gmail.com>
2017-04-22 16:49:11 -0500

    finished breadth-first search

--- ruby_programming/computer_science/binary_tree/binary_tree.rb
+++ ruby_programming/computer_science/binary_tree/binary_tree.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Node
   attr_accessor :value, :parent, :children
 
@@ -21,6 +23,8 @@ class Node
 
 end
 
+
+
 def find_parent(root,leaf)
   if leaf.value > root.value && root.children[:right] == nil
     root.children[:right] = leaf
@@ -43,13 +47,37 @@ def build_tree(arr)
   arr[1..-1].each do |element|
     node = Node.new
     node.value = element
-      find_parent(tree_root,node)
+    find_parent(tree_root,node)
   end
-  puts tree_root.children
+    return tree_root
 end
 
+def breadth_first_search(item, arr)
+  tree_root = build_tree(arr)
+  item_location = nil
+  search_array = [tree_root]
+  check_value = search_array[0]
+  until check_value.children.values.all? {|value| value == nil}
+    if item == check_value.value
+      item_location = check_value
+      break
+    else
+      search_array = search_array[1..-1]
+      check_value.children.values.compact.each do |child|
+        search_array << child
+      end
+      check_value = search_array[0]
+    end #end of if statement
+  end #end of until loop
+  if item_location == nil
+    puts "Item does not exist in array"
+  else
+    puts "Item is located in #{item_location}"
+    puts "Item value: #{item_location.value}"
+  end
+end
 
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-build_tree(arr)
+breadth_first_search(4,arr)
